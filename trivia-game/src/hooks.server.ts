5b778989d7a823586e1a111184fcbcbe345e4ea1
EDVERI03 <89509976+EDVERI03@users.noreply.github.com>
2023-06-06 15:04:18 +0200

    not sure

--- trivia-game/src/hooks.server.ts
+++ trivia-game/src/hooks.server.ts
@@ -28,4 +28,5 @@ export const handle: Handle = async ({ event, resolve }) => {
 	const response = await resolve(event);
 	response.headers.append("Access-Control-Allow-Origin", `*`);
 	return response;
+	
 };
