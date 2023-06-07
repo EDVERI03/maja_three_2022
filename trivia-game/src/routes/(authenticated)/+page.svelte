7d0dbebda4d78233877bac0da50d02f1f2a044d6
EDVERI03 <89509976+EDVERI03@users.noreply.github.com>
2023-06-07 14:30:30 +0200

    Psychology ;)

--- trivia-game/src/routes/(authenticated)/+page.svelte
+++ trivia-game/src/routes/(authenticated)/+page.svelte
@@ -12,11 +12,11 @@
 <div class="mainbox ">
     <TitleCard></TitleCard>
     <div class="buttonholderbox">
-        <a href="/solo-rush" style="width:inherit; max-width:inherit;">
-            <button type="button" class="buttonbox cyan-button">SOLO RUSH</button>
-        </a>
         <a href="/survival" style="width:inherit; max-width:inherit;">
             <button type="button" class="buttonbox sky-button">SURVIVAL</button>
+        </a>
+        <a href="/solo-rush" style="width:inherit; max-width:inherit;">
+            <button type="button" class="buttonbox cyan-button">SOLO RUSH</button>
         </a>
     </div>
 </div>
