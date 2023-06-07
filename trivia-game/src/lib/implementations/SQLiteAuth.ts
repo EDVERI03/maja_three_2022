38d009440d9c99d29f3ca0e3899f8fb7272681f8
EDVERI03 <89509976+EDVERI03@users.noreply.github.com>
2023-06-07 09:00:55 +0200

    Can I register now????!?!?

--- trivia-game/src/lib/implementations/SQLiteAuth.ts
+++ trivia-game/src/lib/implementations/SQLiteAuth.ts
@@ -5,16 +5,16 @@ import * as crypto from "crypto"
 export class SQLiteAuth implements Auth {
     async login(form: FormData): Promise<LoginResult> {
 
-        const username = form.get("username")!.toString()
+        const email = form.get("email")!.toString()
         const password = form.get("password")!.toString()
 
         try {
-            const user = await database.user.findUniqueOrThrow({where: {username}})
+            const user = await database.user.findUniqueOrThrow({where: {email}})
             const comphash = crypto.pbkdf2Sync(password.toString()??"", user.salt, 1000, 64, 'sha512').toString('hex')
     
             if (comphash == user.hash) {
                 const session = crypto.randomUUID()
-                await database.user.update({where: {username}, data:{session}})
+                await database.user.update({where: {email}, data:{session}})
                 return {success: {session}}
             }
 
@@ -27,18 +27,16 @@ export class SQLiteAuth implements Auth {
     }
 
     async register(form: FormData): Promise<LoginResult> {
-        console.log("help")
+
         const username = form.get("username")!.toString()
+
         const password = form.get("password")!.toString()
         const repeat = form.get("repeat")!.toString()
         if (password != repeat) {
-            console.log("me")
             return {error: {code:400, data: "Password Not Repeated Correctly."}}
         }
-        if ( username != undefined && password != undefined) {
-            console.log("I'm")
+        if (username != undefined && password != undefined) {
             if (!(await database.user.findUnique({where: {username}}))) {
-                console.log("Dying")
                 const salt = crypto.randomBytes(16).toString('hex');
                 const hash = crypto.pbkdf2Sync(password.toString()??"", salt, 1000, 64, 'sha512').toString('hex')
                 const session = crypto.randomUUID()
